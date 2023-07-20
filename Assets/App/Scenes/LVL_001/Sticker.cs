9b1334621734e5338e4b9a729d72e30e576dcb39
ajimeneza <ajimeneza@edu.tecnocampus.cat>
2023-07-20 16:04:20 +0200

    GreenCityStructure Level

---
+++ Assets/App/Scenes/LVL_001/Sticker.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sticker : MonoBehaviour
+{
+    Vector3 origin;
+
+    private void Start()
+    {
+        origin = transform.position;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
