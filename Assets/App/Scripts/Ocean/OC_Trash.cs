6cf5e964a5b9355f13f5d66c5dc4d7c18e7a8747
Afrojuan <jdrcdj@gmail.com>
2023-07-22 22:59:11 -0400

    stashed changes

--- Assets/App/Scripts/Ocean/OC_Trash.cs
+++ Assets/App/Scripts/Ocean/OC_Trash.cs
@@ -1,8 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class OC_Trash : MonoBehaviour
+namespace App.Scripts.Ocean
 {
-    
+    public class OC_Trash : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+
+            if (col.CompareTag("Sticker"))
+            {
+                FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Beach_Level/Oil Collected");
+                Destroy(col.gameObject);
+            }
+        }
+    }
 }
