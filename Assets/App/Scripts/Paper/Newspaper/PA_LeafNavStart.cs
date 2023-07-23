5a584c670a44be75c74ff96d9106bae0394b77a0
Juan Deyby Carlos-Chullo <deyby57@gmail.com>
2023-07-23 11:52:53 -0500

    feat: first version Ocean

---
+++ Assets/App/Scripts/Paper/Newspaper/PA_LeafNavStart.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using App.Scripts.Paper.Newspaper;
+using Lean.Gui;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PA_LeafNavStart : MonoBehaviour
+{
+    [SerializeField] private LeanButton button;
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private PA_Leaf leaf;
+
+    private void Awake()
+    {
+        button.OnClick.AddListener(OnClick);
+    }
+
+    void OnClick(){
+
+        if (leaf.isLeft)
+        {
+            Debug.Log("Left");
+            leaf.Newspaper.OnLeft();
+            leaf.isLeft = false;
+            Hide();
+        }
+    }
+    
+    private void Show()
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+    
+    private void Hide()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+}
--- Assets/App/Scripts/Paper/Newspaper/PA_Page.cs
+++ Assets/App/Scripts/Paper/Newspaper/PA_Page.cs
@@ -7,11 +7,37 @@ public class PA_Page : MonoBehaviour
 {
     [SerializeField] private PA_Leaf leaf;
     [SerializeField] private SpriteRenderer page;
-    
+    [SerializeField] private List<GameObject> children;
+
     public void SetPage()
     {
         var current = page.flipX;
         page.flipX = !current;
         page.sprite = leaf.Newspaper.GetPage();
+        
+        if (page.flipX)
+        {
+            EnableChildren();
+        }
+        else
+        {
+            DisableChildren();
+        }
+    }
+
+    private void EnableChildren()
+    {
+        foreach (var child in children)
+        {
+            child.SetActive(true);
+        }
+    }
+    
+    private void DisableChildren()
+    {
+        foreach (var child in children)
+        {
+            child.SetActive(false);
+        }
     }
 }
